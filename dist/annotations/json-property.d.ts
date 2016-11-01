536105caaa10b0a69f8077bdff11cf081f3a43bb
Daniel Worsnup <daniel.worsnup@forrent.com>
2016-11-01 09:46:04 -0400

    Remove dist folder.

--- dist/annotations/json-property.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function JsonProperty(path?: string): (target: any, propertyKey: string) => void;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import 'core-js/es7/reflect';
-export * from './annotations';
-export * from './object-mapper.class';
