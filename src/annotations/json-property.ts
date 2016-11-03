9d33f798c1e7956bb240a2523fbe54284f6d143a
Daniel Worsnup <daniel.worsnup@forrent.com>
2016-11-03 15:18:58 -0400

    Add constant for metadata keys

--- src/annotations/json-property.ts
+++ src/annotations/json-property.ts
@@ -1,4 +1,5 @@
 import Map = require('core-js/es6/map');
+import { MetadataKeys } from '../constants/';
 import { JsonPropertyOptions } from './';
 
 export function JsonProperty(path?: string, options?: JsonPropertyOptions) {
@@ -15,7 +16,7 @@ export function JsonProperty(path?: string, options?: JsonPropertyOptions) {
         target.pathMapping = target.pathMapping || new Map<string, string[]>();
         target.pathMapping.set(propertyKey, paths);
 
-        const type = Reflect.getMetadata("design:type", target, propertyKey);
+        const type = Reflect.getMetadata(MetadataKeys.TYPE, target, propertyKey);
         target.typeMapping = target.typeMapping || new Map<string, Function>();
         target.typeMapping.set(propertyKey, type);
     };
---
+++ src/constants/metadata-keys.constant.ts
@@ -0,0 +1,4 @@
+export const MetadataKeys = {
+    TYPE: 'design:type',
+    ARRAY_TYPE: 'deserialize:arrayType',
+};
